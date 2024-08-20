8c166a27a4504551dfff7f625a8a10a7daf035e9
Jeet Majumdar <bonimajumdar2003@gmail.com>
2024-08-20 21:41:20 +0530

    Day - 3 Add HeroSection component and update tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center bg-cyan-600">Hello! Next JS</h1>
+      <HeroSection />
     </main>
   );
 }
